576ce1a94051923fb71b805e85d7272b079493c3
Alexandru Thomae <alex@thom.ae>
2017-08-31 22:46:32 +0200

    Generating the changelog from git. Fixed artifact matching.

--- build.sh
+++ build.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
-VERSION="1.0.0-$(date +%s)+$(git rev-parse --short HEAD)"
+
+VERSION="${CI_BUILD_REF_NAME}"
+
+PKGNAME=`cat debian/control | grep '^Package: ' | sed 's/^Package: //'`
+TAGS=($(git tag -l v*|sort -rV))
+
+>debian/changelog
+for ((i=0; i < ${#TAGS[@]}; i++)); do
+    tag=${TAGS[i]}
+    if [ "${next_tag} " == " " ]; then
+      VERSION=${tag#v}
+    fi
+    next_tag=${TAGS[i+1]}
+    echo -e "$PKGNAME (${tag#v}) xenial; urgency=medium\n" >> debian/changelog
+    if [ "${next_tag} " == " " ]; then
+        git log --pretty=format:'  * %s' $tag >> debian/changelog
+        git log --pretty='format:%n%n -- %aN <%aE>  %aD%n%n' $tag >> debian/changelog
+    else
+        git log --pretty=format:'  * %s' $next_tag..$tag >> debian/changelog
+        git log --pretty='format:%n%n -- %aN <%aE>  %aD%n%n' $tag^..$tag >> debian/changelog
+    fi
+    PREVTAG=$tag
+done
+
 sed -i -e "s/{{VERSION}}/${VERSION}/g" setup.py
-sed -i -e "s/{{VERSION}}/${VERSION}/g" debian/changelog
 sed -i -e "s/{{VERSION}}/${VERSION}/g" debian/rules
 
 mk-build-deps
@@ -9,6 +31,9 @@ dpkg --unpack geth-exporter*build-deps*.deb
 git clean -xfd
 
 debian/rules make-orig-tar
-mkdir -p build
-mv *.orig.tar* build/
 dpkg-buildpackage -us -uc
+
+rm -rf package/
+mkdir -p package/
+mv *.orig.tar* package/
+mv ../geth-exporter_* package/
--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ eval $(ssh-agent -s)
 ssh-add <(echo "${SSH_PRIVATE_KEY}")
 mkdir -p ~/.ssh
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
-scp build/geth-exporter*.deb aptly@${REPOSITORY_SERVER}:/var/lib/aptly/store
+scp package/geth-exporter*.deb aptly@${REPOSITORY_SERVER}:/var/lib/aptly/store
 ssh aptly@${REPOSITORY_SERVER} /usr/bin/aptly add
 ssh aptly@${REPOSITORY_SERVER} /usr/bin/aptly publish
